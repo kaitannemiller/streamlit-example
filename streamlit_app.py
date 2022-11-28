7a32c0c788c03eb91e867ab4fd7f9e9fbdc60d06
kaitannemiller <65577738+kaitannemiller@users.noreply.github.com>
2022-11-27 23:37:10 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,12 @@ style ="""
             border-style: outset;
         }
     </style>
+    <style>
+        #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > .streamlit-expander > li > .streamlit-expanderHeader > div > p {
+            font-size: 24px;
+            font-weight: 700;
+        }
+    </style>
     <!-- Move down the feature selection area -->
     <style>
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div {
