184a18d927b7b5dc8220b4b68ce12da938a5a057
kaitannemiller <65577738+kaitannemiller@users.noreply.github.com>
2022-11-28 00:03:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,6 +194,8 @@ style ="""
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(2) > div > div > div > div {
             width: 100% !important;
         }
+        #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div > div > div > div > div > div > div,
+        #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div > div > div > div > div > div > div > div,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div > div > div > div > div > div > div > div > div,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div > div > div > div > div > div > div > div > div > div > div,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div > div > div > div > div > div > div > div > div > div > div > div,
