97d21ffbfd505e7c1a6f5a87e9708ab870495c4d
kaitannemiller <65577738+kaitannemiller@users.noreply.github.com>
2022-12-02 22:42:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,7 +174,7 @@ style ="""
     <style>
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div > div > div > div > div > div > div > div > div.e1tzin5v0 > div > div > label,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div > div > div > div > div > div > div > div > div > label,
-        #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div.streamlit-expanderContent > div > div > div > div > div > div > div > div > div > div > div > div > div > label {
+        #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div.streamlit-expanderContent > div > div > div > div > div > div > div > div > div > div > div > div > div > label > div > p {
             min-width: max-content;
             padding-right: 10px;
             margin-bottom: 0px;
