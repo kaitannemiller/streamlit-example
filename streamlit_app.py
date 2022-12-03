a44e7fa63ef65423e1e31b4b91e16b4a5f379d4f
kaitannemiller <65577738+kaitannemiller@users.noreply.github.com>
2022-12-02 22:56:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,6 +159,7 @@ style ="""
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(1) > div > div > div > div > div {
             width: 100% !important;
         }
+            #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(1) > div > div > div > div > div > div,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(1) > div > div > div > div > div > div ,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(1) > div > div > div > div > div > div > div,
         #root > div > div.withScreencast > div > div > div > section > div > div > div > div > div > div > ul > li > div.streamlit-expanderContent > div > div > div > div > div.epcbefy1:nth-child(1) > div > div > div > div > div > div > div > div {
